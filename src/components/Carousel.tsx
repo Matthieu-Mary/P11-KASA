adaab3a94fc79d1540d64fb05229fcdf828f0160
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-11 09:53:17 +0100

    add carousel component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,34 @@
-type CarouselPropsType = {
-    pictures: string[];
-}
+import { useState } from "react";
+import Arrow from "../assets/icons/ArrowCarousel.svg";
 
-export const Carousel: React.FC<CarouselPropsType> = ({pictures}) => {
+type CarouselPropsType = {
+  pictures: string[];
+};
+
+export const Carousel: React.FC<CarouselPropsType> = ({ pictures }) => {
+  const [imageNumber, setImageNumber] = useState(0);
+
+  const handlePrevious = () => {
+    if(imageNumber <= 0) {
+      setImageNumber(pictures.length - 1)
+    } else {
+      setImageNumber(imageNumber - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if(imageNumber >= pictures.length - 1) {
+      setImageNumber(0)
+    } else {
+      setImageNumber(imageNumber + 1)
+    }
+  }
+
   return (
     <div className="carousel">
-        <img src={pictures[0]} alt="" />
+      <img onClick={handlePrevious} className="arrow-left" src={Arrow} alt="previous image" />
+      <img className="picture" src={pictures[imageNumber]} alt="apartment picture" />
+      <img onClick={handleNext} className="arrow-right" src={Arrow} alt="next image" />
     </div>
-  )
-}
+  );
+};
--- src/pages/Homepage/Apartment.tsx
+++ src/pages/Homepage/Apartment.tsx
@@ -1,36 +1,45 @@
 import { useParams } from "react-router-dom";
 import { Carousel } from "../../components/Carousel";
-import data from "../../data/data.json"
+import data from "../../data/data.json";
 import { Tag } from "../../components/Tag";
-import { Star } from "../../components/Star";
+import starImg from "../../assets/images/star.svg";
 
 export const Apartment = () => {
-
-    const { id } = useParams();
-    const rightApartment:any = data.find(apartment => apartment.id === id);
-    console.log(rightApartment)
+  const { id } = useParams();
+  const rightApartment: any = data.find((apartment) => apartment.id === id);
 
   return (
     <main className="apartment">
-        <Carousel pictures={rightApartment.pictures} />
-        <div className="container-apartment-infos">
-            <div className="titles-tags">
-            <h1>{rightApartment.title}</h1>
-            <h3>{rightApartment.location}</h3>
-            {
-                rightApartment.tags.map((tag:string, index:number) => <Tag text={tag} key={index} />)
-            }
-            </div>
-            <div className="host">
-                <div className="infos-host">
-                    <p>{rightApartment.host.name}</p>
-                    <img src={rightApartment.host.picture} alt={`${rightApartment.host.name} profil picture`} />
-                </div>
-                <div className="stars">
-                    {[1,2,3,4,5].map(star => <Star key={star}/>)}
-                </div>
-            </div>
+      <Carousel pictures={rightApartment.pictures} />
+      <div className="container-apartment-infos">
+        <div className="titles-tags">
+          <h1>{rightApartment.title}</h1>
+          <h3>{rightApartment.location}</h3>
+          <div className="container-tags">
+            {rightApartment.tags.map((tag: string, index: number) => (
+              <Tag text={tag} key={index} />
+            ))}
+          </div>
         </div>
+        <div className="host">
+          <div className="infos-host">
+            <p>{rightApartment.host.name}</p>
+            <img
+              src={rightApartment.host.picture}
+              alt={`${rightApartment.host.name} profil picture`}
+            />
+          </div>
+          <div className="stars">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <img
+                className={star <= rightApartment.rating ? "colorized" : ""}
+                src={starImg}
+                alt="star"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
